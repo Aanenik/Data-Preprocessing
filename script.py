09291027d8f82020fc21e93fe23433a85ea7bf1d
Aanenik <aanenik21@gmail.com>
2025-05-10 16:25:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,4 +4,4 @@ from sklearn.preprocessing import StandardScaler # type: ignore
 df=pd.read_csv("C:/Users/ANUJA NILAK/Downloads/DATAMITES DOCS/GIT & GITHUB/gitpractice/all_files/file.csv")
 sc=StandardScaler()
 df_final= sc.fit_transform(df)
-print(df_final)
+# print(df_final)
